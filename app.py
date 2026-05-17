4ccda9933add6bd6682d761de64454be88469a80
SdwByte <sdwbyte@easy-park-pay.xyz>
2026-05-17 21:45:11 +0300

    security: secure cookie flags (Secure/HttpOnly/SameSite)

--- app.py
+++ app.py
@@ -114,6 +114,11 @@ def create_app():
     db_path = os.environ.get('DATABASE_URL', 'sqlite:///easypark.db')
     app.config['SQLALCHEMY_DATABASE_URI'] = db_path
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SESSION_COOKIE_SECURE'] = True
+    app.config['SESSION_COOKIE_HTTPONLY'] = True
+    app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
+    app.config['REMEMBER_COOKIE_SECURE'] = True
+    app.config['REMEMBER_COOKIE_HTTPONLY'] = True
 
     db.init_app(app)
     socketio.init_app(app, cors_allowed_origins='*', async_mode='gevent')
